c5dc9d4d63ad08aef83915d06cdd31c0089d14bc
vanessalsq <leishu_qiu@brown.edu>
2024-04-08 23:28:22 -0400

    ver1

--- distribution/all/mr.js
+++ distribution/all/mr.js
@@ -1,6 +1,10 @@
-const mr = function(config) {
+const distribution = require('../../distribution');
+const id = require('../util/id');
+
+const mr = function (config) {
   let context = {};
   context.gid = config.gid || 'all';
+  context.hash = config.hash || id.naiveHash;
 
   return {
     exec: (config, callback) => {
@@ -8,26 +12,6 @@ const mr = function(config) {
       let mapped = [];
       let reduced = [];
       let cnt = 0;
-      console.log('keys ??', keys);
-      keys.forEach((key) => {
-        let mapResult;
-        global.distribution[context.gid].store.get(key, (e, v) => {
-          cnt++;
-          if (e) {
-            callback(new Error('store get fail'));
-          } else {
-            mapResult = map(key, v);
-            if (Array.isArray(mapResult)) {
-              mapped.push(...mapResult);
-            } else {
-              mapped.push(mapResult);
-            }
-            if (cnt == keys.length) {
-              shuffleReduce();
-            }
-          }
-        });
-      });
 
       function shuffleReduce() {
         console.log('!!map', mapped);
@@ -45,8 +29,93 @@ const mr = function(config) {
         reduced = Object.keys(shuffle).map((k) => reduce(k, shuffle[k]));
         callback(null, reduced);
       }
+
+      global.distribution[context.gid].status.get('nid', (e, nids) => {
+        global.distribution.local.groups.get(context.gid, (e, nodes) => {
+          keys.map((key) => {
+            const kid = id.getID(key);
+            const hashVal = context.hash(kid, nids).substring(0, 5);
+            const mapperNode = nodes[hashVal];
+            const putMapRemote = {
+              service: 'routes',
+              method: 'put',
+              node: {ip: mapperNode.ip, port: mapperNode.port},
+            };
+
+            global.distribution.local.comm.send(
+              [map, 'mapService'],
+              putMapRemote,
+              (e, v) => {
+                const storeGetRemote = {
+                  service: 'store',
+                  method: 'get',
+                  node: {ip: mapperNode.ip, port: mapperNode.port},
+                };
+                global.distribution.local.comm.send(
+                  [{key: key, gid: context.gid}],
+                  storeGetRemote,
+                  (e, value) => {
+                    console.log('!!', value);
+
+                    const getMapRemote = {
+                      service: 'routes',
+                      method: 'get',
+                      node: {ip: mapperNode.ip, port: mapperNode.port},
+                    };
+                    global.distribution.local.comm.send(
+                      ['mapService'],
+                      getMapRemote,
+                      (e, service) => {
+                        console.log('??', service.toString());
+
+                        const mapResult = service(key, value);
+                        console.log('@@', mapResult);
+                        const storePutRemote = {
+                          service: 'store',
+                          method: 'put',
+                          node: {ip: mapperNode.ip, port: mapperNode.port},
+                        };
+
+                        if (Array.isArray(mapResult)) {
+                          mapped.push(...mapResult);
+                        } else {
+                          mapped.push(mapResult);
+                        }
+                        cnt++;
+                        if (cnt == keys.length) {
+                          shuffleReduce();
+                        }
+                      },
+                    );
+                  },
+                );
+              },
+            );
+          });
+        });
+      });
     },
   };
 };
 
+// console.log('keys ??', keys);
+// keys.forEach((key) => {
+//   let mapResult;
+//   global.distribution[context.gid].store.get(key, (e, v) => {
+//     cnt++;
+//     if (e) {
+//       callback(new Error('store get fail'));
+//     } else {
+//       mapResult = map(key, v);
+//       if (Array.isArray(mapResult)) {
+//         mapped.push(...mapResult);
+//       } else {
+//         mapped.push(mapResult);
+//       }
+//       if (cnt == keys.length) {
+//         shuffleReduce();
+//       }
+//     }
+//   });
+// });
 module.exports = mr;
